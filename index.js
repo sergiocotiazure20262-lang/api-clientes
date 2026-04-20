99fdabef96e4d64359e5673d290237fdfbb36d83
sergiocotiazure20262-lang <sergiocotiazure20262@gmail.com>
2026-04-20 09:04:18 -0300

    Configuração do postgresql e containers do Docker

--- index.js
+++ index.js
@@ -2,12 +2,25 @@
 const express = require('express');
 const swagger = require('swagger-ui-express');
 const swaggerDoc = require('swagger-jsdoc');
+const { Pool } = require('pg');
+
+//Carregando as variáveis de ambiente do arquivo .env
+require('dotenv').config();
 
 //Inicializando a aplicação do Express
 const app = express();
 
 //Configurando o Express para trabalhar em modo 'json'
 app.use(express.json());
+
+//Conectando no banco de dados do PostgreSQL (Pool)
+const pool = new Pool({ //Ler as variáveis de ambiente
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
 
 //Configurar o Swagger
 const options = {
@@ -28,6 +41,28 @@ const options = {
 //Executando a documentação do Swagger
 const specs = swaggerDoc(options);
 app.use('/docs', swagger.serve, swagger.setup(specs));
+
+//Criando uma tabela no banco de dados para salvar os clientes
+async function inicializarBanco() {
+    //Verificar se o banco de dados não possui uma tabela de cliente
+    //e caso não tenha a tabela iremos criar a tabela
+    await pool.query(`
+            CREATE TABLE IF NOT EXISTS clientes(
+                id SERIAL PRIMARY KEY,
+                nome VARCHAR(100),
+                email VARCHAR(50) UNIQUE
+            )
+        `);
+
+    //Cadastrar 2 clientes incialmente na tabela
+    await pool.query(`
+            INSERT INTO clientes (nome, email)
+            VALUES
+                ('Ana Maria', 'anamaria@gmail.com'),
+                ('João Pedro', 'joaopedro@gmail.com')
+            ON CONFLICT (email) DO NOTHING
+        `);
+}
 
 //Estrutura de dados para armazenar os clientes em memória
 let clientes = [ //array de objetos
@@ -219,9 +254,16 @@ app.delete("/api/clientes/:id", (req, res) => {
     });
 });
 
-
-//Executar o servidor da aplicação
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
-    console.log('Swagger rodando em http://localhost:3000/docs');
-});
+//Executar a conexão e incialização do banco de dados
+inicializarBanco()
+    .then(() => { //Executa caso o 'inicializarBanco' dê sucesso!
+        //Executar o servidor da aplicação
+        app.listen(3000, () => {
+            console.log('Servidor rodando em http://localhost:3000');
+            console.log('Swagger rodando em http://localhost:3000/docs');
+            console.log('PostgreSQL conectado na porta 5432');
+        });
+    })
+    .catch((error) => { //Executa caso o 'inicializarBanco' dê erro!
+        console.log('Erro ao inicializar o banco de dados: ', error.message);
+    });
